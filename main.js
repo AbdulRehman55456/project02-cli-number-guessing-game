eb30dd87362f4ca45e9fa3ab3a264b256846365f
AbdulRehman55456 <siontoji@gmail.com>
2024-04-23 23:56:26 -0700

    inki pinki

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-// 1) computer will generate a random number 
-// 2) user input for guessing number 
+// 1) computer will generate a random number
+// 2) user input for guessing number
 // 3) compare user input with computer generated number and show result
-const randomnumber = Math.floor(Math.random() * 10 + 1);
-//console.log(randomnumber);
-const answers = await inquirer.prompt([
-    {
-        name: "userguessednumber",
-        type: "number",
-        message: "please guess a number between 1-10:",
+let condition = true;
+while (condition) {
+    const randomnumber = Math.floor(Math.random() * 10 + 1);
+    //console.log(randomnumber);
+    const answers = await inquirer.prompt([
+        {
+            name: "userguessednumber",
+            type: "number",
+            message: "please guess a number between 1-10:",
+        },
+    ]);
+    if (answers.userguessednumber === randomnumber) {
+        console.log("you win");
     }
-]);
-if (answers.userguessednumber === randomnumber) {
-    console.log("you win");
-}
-else {
-    console.log("you lose");
+    else {
+        console.log("you lose");
+    }
 }
--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
-// 1) computer will generate a random number 
-// 2) user input for guessing number 
+// 1) computer will generate a random number
+// 2) user input for guessing number
 // 3) compare user input with computer generated number and show result
- 
-const randomnumber = Math.floor(Math.random()*10+1);
-//console.log(randomnumber);
+let condition = true;
+while (condition) {
+  const randomnumber = Math.floor(Math.random() * 10 + 1);
+  //console.log(randomnumber);
 
-const answers = await inquirer.prompt([
+  const answers = await inquirer.prompt([
     {
-        name : "userguessednumber",
-        type : "number",
-        message :"please guess a number between 1-10:",
-    }
-]);
+      name: "userguessednumber",
+      type: "number",
+      message: "please guess a number between 1-10:",
+    },
+  ]);
 
-if( answers.userguessednumber === randomnumber){
+  if (answers.userguessednumber === randomnumber) {
     console.log("you win");
-}else{
+  } else {
     console.log("you lose");
+  }
 }
